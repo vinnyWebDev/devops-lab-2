a663f114a08d05d5330790edb96a803727071f3b
vinnyWebDev <vinnywebdev@gmail.com>
2023-06-10 13:29:18 +0100

    final commit

--- script.sh
+++ script.sh
@@ -1,2 +1,14 @@
 #!/bin/bash
-rm -r fol_1 fol_2
+mkdir fol_1 fol_2
+cd fol_1
+touch 1_1.txt 1_2.txt 1_3.txt
+chmod 411 1_1.txt 1_3.txt 
+chmod 444 1_2.txt
+cd ..
+cd fol_2
+touch 2_1.txt 2_2.txt 2_3.txt
+chmod 411 2_1.txt 2_3.txt 
+chmod 444 2_2.txt 
+touch 2_1.txt 2_2.txt 2_3.txt 
+
+echo "Job completed"
